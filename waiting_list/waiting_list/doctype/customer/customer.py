17c302ace60f5862a3319e0c0441f75af84063e5
Fadil <Fadil>
2021-01-21 05:47:38 +0000

    comments added

--- waiting_list/waiting_list/doctype/customer/customer.py
+++ waiting_list/waiting_list/doctype/customer/customer.py
@@ -9,29 +9,34 @@ import random
 
 class Customer(Document):
 	def codeGenerator(self):
-		
-		code_name=self.phone_number[-4:]
+
+		##Random code is generated using upper and lower case alphabets,numbers and last four digits of new customer's phone number
+
+		code_name=self.phone_number[-4:] ##slice last 4 digits of customer phone number
 				
 		upper = "QWERTYUIOPASDFGHJKLZXCVBNM"
 		numbers = "1234567890"
 		lower = "qwertyuiopasdfghjklzxcvbnm"
 		total = upper+numbers+lower
-		length = 8
-		self.referral = "".join(random.sample(total,length))
-		self.referral_code=self.referral+code_name
+		length = 8  ##length of code is defined to 8 
+		self.referral = "".join(random.sample(total,length)) ##randomly generates code
+		self.referral_code=self.referral+code_name ##last four digits of phone number is added to the randomly generated code
 		return self.referral_code
 		
 	def before_submit(self):
-		self.referral_id = self.codeGenerator()
+		self.referral_id = self.codeGenerator() ##randomly generated code passed to referral id field of new customer upon submission of document
+
+		##if new customer is created using existing customers referral code, phone number of existing customer whose referral id is used will be passed to referred by field of new customer
+
 		customer_doc = frappe.db.get_all('Customer',fields=['customer','referral_id','phone_number','referred_by', 'name'])
-	
+
 		for i in customer_doc:
 			if self.enter_referral_code:
-				if i['referral_id'] == self.enter_referral_code:
-					self.referred_by = i['phone_number']
-					self.customer =i['name']
+				if i['referral_id'] == self.enter_referral_code: ##checks if enterd referral code matches with existing referral ids 
+					self.referred_by = i['phone_number']  ##if it is matched, phone number of existing customer whose referral id is used will be passed to referred by field of new customer
+					self.customer =i['name'] ##customer id of existing customer is passed to the name field of doc
 				
-
+				##upon creation of new customer who used referral id's of existing customers,new customers mobile number is added to the referred to table of existing customer whose referral id is used by new customer.
 					doc=frappe.get_doc('Customer',self.customer)
 
 					doc.append('referred_to', {
